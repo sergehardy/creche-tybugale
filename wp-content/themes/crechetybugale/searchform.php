e4d6a4a5a6360a2ffbc59b6336f97bbcac47f502
Serge HARDY <serge.hardy@zenika.com>
2019-10-15 23:19:16 +0200

    Init WP plugin

---
+++ wp-content/themes/crechetybugale/searchform.php
@@ -0,0 +1,4 @@
+<form action="/" method="get">
+    <input type="text" name="s" id="search"  onfocus="this.value=''" value="Rechercher<?php //the_search_query(); ?>" />
+<!--    <input type="image" alt="Rechercher" src="--><?php //bloginfo( 'template_url' ); ?><!--/images/search.png" />-->
+</form>
